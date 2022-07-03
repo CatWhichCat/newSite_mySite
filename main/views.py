a775718be9545cbe99c9619e2c5b98dd00cc527f
CatWhichCat <fortunalich@mail.ru>
2022-07-03 13:09:13 +0600

    view, form, create.html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
-
+from .forms import *
 
 def index(request):
     task = Task.objects.all()
@@ -12,4 +12,19 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
-    return render(request, 'main/create_task.html')
+    error = ''
+    
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            error = 'Форма была неверной'
+    
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'main/create_task.html', context)
